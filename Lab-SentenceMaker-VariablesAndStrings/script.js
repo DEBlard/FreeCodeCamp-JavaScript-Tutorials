dcd1623732e9099b88cbba01aeddeb0155393bc4
DEBlard <sibongile.dlamini324@gmail.com>
2026-05-12 19:57:12 +0200

    Add sentence generation with variables in script.js

---
+++ Lab-SentenceMaker-VariablesAndStrings/script.js
@@ -0,0 +1,19 @@
+let adjective,noun,verb,place,adjective2,noun2;
+adjective = "happy";
+noun = "Thabo";
+verb = "dancing";
+place = "mzansi";
+adjective2 = "cheerful";
+noun2 = "bananas";
+
+const firstStory = "Once upon a time, there was a(n) " .concat(adjective, " ", noun, " who loved to eat ", noun2, ". The ", noun, " lived in a ", place, " and had ", adjective2, " nostrils that blew fire when it was ", verb, ".");
+console.log("First story: " + firstStory);
+
+adjective = "noisy";
+noun = "goat";
+verb = "jumping";
+place = "pan";
+adjective2 = "slick";
+noun2 = "sheep";
+const secondStory = "Once upon a time, there was a(n) " .concat(adjective, " ", noun, " who loved to eat ", noun2, ". The ", noun, " lived in a ", place, " and had ", adjective2, " nostrils that blew fire when it was ", verb, ".");
+console.log("Second story: " + secondStory)
