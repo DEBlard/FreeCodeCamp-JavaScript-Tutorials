2021333a289f10794e363a144c1be839519d4d2b
DEBlard <sibongile.dlamini324@gmail.com>
2026-05-12 18:52:54 +0200

    Initialize TriviaBot with coding facts

---
+++ Lab-VariablesAndString-TriviaBot/script.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "Sonep";
+const botLocation = "mars";
+const favoriteLanguage = "Python";
+
+console.log("My name is "+ botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was named after a british comedy not the snake.";
+console.log(codingFact);
+codingFact = favoriteLanguage + " is older that Java.";
+console.log(codingFact);
+codingFact = "Guido van Rossum created" + favoriteLanguage + " as a hobby during Christmas holidays.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
